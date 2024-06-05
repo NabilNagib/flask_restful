bf01c45983e4b17908d9f19dfab1460da1781a77
NabilNagib <nnabyl@gmail.com>
2024-06-05 13:02:15 +0300

    db

--- server/app.py
+++ server/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from models import db, Newsletter
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletters.db'
